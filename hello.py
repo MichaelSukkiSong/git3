e6745273b2929835f5cd3860144081d2256b60f3
sa77irak <sa77irak@gmail.com>
2019-04-10 11:50:33 +0900

    feat:functionalize hello.py while __name__ is equal to __main__, do do_loop_print().

--- hello.py
+++ hello.py
@@ -1,6 +1,12 @@
-for i in range(1,5+1):
-    if i%2==0:
-        print('hello python with git in another dimension.')
-    else:
-        print("Nothing to say")
+#TODO: 아래의 반복문을 함수화 한 후, __main__일 때만 동작하도록 구성하시오.
 
+def do_loop_cs():
+    for i in range(1,5+1):
+        if i%2==0:
+            print('hello python with git in another dimension.')
+        else:
+            print("Nothing to say")
+
+
+if __name__=='__main__':
+    do_loop_cs();
